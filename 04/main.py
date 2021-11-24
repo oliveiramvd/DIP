27ac953b8824d1c6e9bdc9b8e82cf1913c3b1f94
Marcos Oliveira <marcoskira@users.noreply.github.com>
2021-11-24 07:02:38 -0300

    04 final updates

--- 04/main.py
+++ 04/main.py
@@ -1,25 +1,25 @@
-import sys
 import math
 import numpy as np
 import cv2
 
 from matplotlib import pyplot as plt
+from numpy.core.fromnumeric import std
 from classes import Pixel
 from classes import Blob
 
-INPUT_IMAGE = "img/60.bmp"
+INPUT_IMAGE = ["img/60.bmp", "img/82.bmp", "img/114.bmp", "img/150.bmp","img/205.bmp"]
 MIN_PIXELS_N = 15               # Minimum blob pixel quantity
 FOREGROUND = 255                # Color to fill in Foreground (0 - 255)
 BACKGROUND = 0                  # Color to fill in Background (0 - 255)
 
-
 # Calculates the distance in color spectrum between two pixels for BGR channels.
-def calculateDistance(img, seed_x, seed_y, child_x, child_y):
+def calculates_distance(img, seed_x, seed_y, child_x, child_y):
     seed_pixel = img[seed_y][seed_x]
     child_pixel = img[child_y][child_x]
 
     return abs(int(seed_pixel) - int(child_pixel))
 
+# Finds the minimum and maximum positions of the edge pixels of the region of interest in the image
 def find_min_max_pixel_position(blob, pixel):
     
         # Mininum X and Y
@@ -38,9 +38,8 @@ def find_min_max_pixel_position(blob, pixel):
         
         return blob
 
-
 # Creates an auxiliar 2Dimensional array which will be used for pixel labelling
-def createsAuxMatrix(width, height):
+def creates_aux_matrix(width, height):
     row = []
     cols = []
 
@@ -55,16 +54,16 @@ def createsAuxMatrix(width, height):
     return cols
 
 # Detects blobs on a given image
-def blobDetection(img, min_pixels_n):
+def blob_detection(img, min_pixels_n):
     height, width = img.shape[0], img.shape[1]
     stack = []
     list_of_blobs = []
-    pixels = createsAuxMatrix(width, height)
+    pixels = creates_aux_matrix(width, height)
     label = 0
     pixels_per_blob = 0
     blob_px_qty = []
 
-    # store most extreme pixels from the image in axis x and y
+    # Store edge pixels within the image
     global_min_max_pos = {'min': {'x': width-1, 'y': height-1 }, 
                         'max': {'x': 0, 'y': 0}}
 
@@ -76,10 +75,10 @@ def blobDetection(img, min_pixels_n):
             if pixels[y][x].label == -1 and pix == FOREGROUND:
                 pixels[y][x].label = label
 
-                stack.append(pixels[y][x]) # Seed pixel in put inside a stack
+                stack.append(pixels[y][x])
                 b = Blob(width, height)
 
-                # While stack is not empty, verify neighbors from pixel which was never visited before
+                # While stack is not empty, verify neighborhood from pixel which was never visited before
                 while len(stack) != 0:
                     p = stack.pop()
 
@@ -89,47 +88,45 @@ def blobDetection(img, min_pixels_n):
                     # checks if this new pixel is a new min/max position in that blob 
                     b = find_min_max_pixel_position(b, p)
 
-                    # Neighboard 4
+                    # Neighborhood 4
                     # Top
                     if p.y - 1 >= 0:
                         if pixels[p.y-1][p.x].label == -1:
-                            dist = calculateDistance(img, p.x, p.y, p.x, p.y-1)
+                            dist = calculates_distance(img, p.x, p.y, p.x, p.y-1)
                             if dist == 0:
                                 pixels[p.y-1][p.x].label = label
                                 stack.append(pixels[p.y-1][p.x])
                     # Right
                     if p.x + 1 < width:
                         if pixels[p.y][p.x+1].label == -1:
-                            dist = calculateDistance(img, p.x, p.y, p.x+1, p.y)
+                            dist = calculates_distance(img, p.x, p.y, p.x+1, p.y)
                             if dist == 0:
                                 pixels[p.y][p.x+1].label = label
                                 stack.append(pixels[p.y][p.x+1])
                     # Bottom
                     if p.y + 1 < height:
                         if pixels[p.y+1][p.x].label == -1:
-                            dist = calculateDistance(img, p.x, p.y, p.x, p.y+1)
+                            dist = calculates_distance(img, p.x, p.y, p.x, p.y+1)
                             if dist == 0:
                                 pixels[p.y+1][p.x].label = label
                                 stack.append(pixels[p.y+1][p.x])
                     # Left
                     if p.x - 1 >= 0:
                         if pixels[p.y][p.x-1].label == -1:
-                            dist = calculateDistance(img, p.x, p.y, p.x-1, p.y)
+                            dist = calculates_distance(img, p.x, p.y, p.x-1, p.y)
                             if dist == 0:
                                 pixels[p.y][p.x-1].label = label
                                 stack.append(pixels[p.y][p.x-1])
 
-
                 # If there is no more pixels in the stack, then all pixels from this blob were found
                 b.pixels_qty = pixels_per_blob
-
 
                 # If blob doesn't have minimum qty of pixels, ignore it
                 if b.pixels_qty >= min_pixels_n:
                     list_of_blobs.append(b)
                     blob_px_qty.append(pixels_per_blob)
 
-                    # checks if within this blob, there is a new min/max global position
+                    # Checks if it's a global min/max edge pixel
                     if global_min_max_pos['min']['x'] > b.min_x:
                         global_min_max_pos['min']['x'] = b.min_x
                     if global_min_max_pos['min']['y'] > b.min_y:
@@ -148,32 +145,7 @@ def blobDetection(img, min_pixels_n):
 
     return global_min_max_pos, blob_px_qty, list_of_blobs
 
-def update_blob_list(blob_qty_px, blob_list):
-    mean = np.mean(blob_qty_px)
-    stdev = np.std(blob_qty_px)
-
-    print(mean, stdev)
-
-    # Percorre toda lista de blobs
-    for k in range(len(blob_list)):
-        # Calcula Z-Score desse blob
-        zscore = math.sqrt(pow(blob_list[k].pixels_qty - mean, 2))/stdev
-        if zscore > 2.5:
-            print("cheguei aqui")
-            # Quantity of times which this blob needs to be divided (cada vez que passa do zscore 3, é um novo blob)
-            qty_divisions = math.ceil(3/zscore)
-
-            for j in range(qty_divisions):
-                new_blob_px_qty = math.floor(blob_list[k].pixels_qty/qty_divisions+1)
-                # Pega valor de label, aumenta +1
-                # Pega aleatoriamente metade dos pixels e cria novo blob pra eles
-                blob_list[k].pixels_qty = new_blob_px_qty
-                new_blob = Blob()
-                new_blob.pixels_qty = new_blob_px_qty
-                blob_list.append(new_blob)
-
-    return blob_list
-            
+# Crop region of interest and resize it within the image (it will be used to adjust all images to have the same ratio)     
 def image_resize(img, global_min_max_pos):
     original_height = img.shape[0]
     original_width = img.shape[1]
@@ -188,49 +160,72 @@ def image_resize(img, global_min_max_pos):
     else:
         new_proportion = diff_height
 
-    print(diff_width, diff_height, new_proportion, img_crop.shape[1], img_crop.shape[0])
-
     img_resize = cv2.resize(img_crop, (int(img_crop.shape[1] + original_width * new_proportion), int(img_crop.shape[0] + original_height * new_proportion)), interpolation=cv2.INTER_LINEAR)
 
     return img_resize
 
+# Adjusts the amount of blobs in each image according to a measure of dispersion
+def adjusts_blobs_from_list(list_blobs_per_image, global_px_per_blob):
+    std_dev = np.std(global_px_per_blob)
+    mean = np.mean(global_px_per_blob)
 
+    for i in range(len(list_blobs_per_image)):
+        # Checks if a blob is greater than a standard deviation
+        for blob in list_blobs_per_image[i]:
+            if blob > mean + std_dev:
 
+                # Breaks a blob until it's within one standard deviation from the mean
+                while blob > mean + std_dev:
+                    blob -= mean
+                    list_blobs_per_image[i].append(mean)
 
-def main ():
-    img = cv2.imread(INPUT_IMAGE, cv2.IMREAD_GRAYSCALE)
-    
+    return list_blobs_per_image
+
+# Filters and binarizes a given image
+def binarization(img):
+    img_copy = img.copy()
+
     # Apply image normalization
     aux_norm = np.zeros((800,800))
-    img_norm = cv2.normalize(img, aux_norm, 0, 255, cv2.NORM_MINMAX)
+    img_norm = cv2.normalize(img_copy, aux_norm, 0, 255, cv2.NORM_MINMAX)
 
-    # Apply meadian blur
+    # Apply median blur
     img_blur = cv2.medianBlur(img_norm,5)
 
     # Adaptive thresholding
-    img_final = cv2.adaptiveThreshold(img_blur,255,cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY,51,-30)
+    img_bin = cv2.adaptiveThreshold(img_blur,255,cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY,51,-30)
 
-    # Change image type
-    #img_final = img_final.astype (np.float32) / 255
+    return img_bin
 
-    # Image segmentation
-    global_min_max_pos, blob_px_qty, list_of_blobs = blobDetection (img_final, MIN_PIXELS_N)
+def main ():
 
-    # Crop region of interest and resize it within the image (it will be used to adjust all images to have the same ratio)
-    img_resize = image_resize(img_final.copy(), global_min_max_pos)
+    img_final = []
+    global_px_per_blob = []
+    list_blobs_per_image = []
 
-    print ('%d componentes detectados. (1)' % len(list_of_blobs))
-    #list_of_blobs = update_blob_list(blob_px_qty, list_of_blobs)
-    #print ('%d componentes detectados. (2)' % len(list_of_blobs))
+    # Reads all images in the list
+    for i in range(len(INPUT_IMAGE)):
+        img = cv2.imread(INPUT_IMAGE[i], cv2.IMREAD_GRAYSCALE)
+        img_bin = binarization(img)
 
-    cv2.imshow('01 - out', img_final)
-    cv2.imshow('02 - out', img_resize)
-    #cv2.imwrite('02 - out.png', img_final*255)
-    cv2.waitKey()
-    cv2.destroyAllWindows()
+        # Segmentation for image resizing (discarted, since it doesn't make difference in the final result anyway~~)
+        #global_min_max_pos, blob_px_qty, list_of_blobs = blob_detection(img_bin, MIN_PIXELS_N)
+        #img_res = image_resize(img_bin.copy(), global_min_max_pos)
+
+        # Image segmentation and gets the global quantity of all blobs from all the images
+        global_min_max_pos, blob_px_qty, list_of_blobs = blob_detection(img_bin, MIN_PIXELS_N)
+
+        global_px_per_blob+= blob_px_qty
+        list_blobs_per_image.append(blob_px_qty)
+
+        img_final.append(img_bin)
+
+    list_blobs_per_image = adjusts_blobs_from_list(list_blobs_per_image, global_px_per_blob)
+
+    # Lists all blobs from all the images
+    for i in range(len(list_blobs_per_image)):
+        print("Image '"+str(INPUT_IMAGE[i]) + "': " +  str(len(list_blobs_per_image[i])))
 
 
 if __name__ == '__main__':
     main ()
-
-#===============================================================================
